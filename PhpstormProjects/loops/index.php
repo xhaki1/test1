32c243f2695db31172c8c2ba37df90b2a936aa83
xhaki <turbac2015@gmail.com>
2018-09-25 19:15:21 +0300

    variables

---
+++ PhpstormProjects/loops/index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: muktar
+ * Date: 25/09/18
+ * Time: 18:42
+ */
+
+//hello world
+$greating= "hello world";
+
+//define contant
+
+define("suprem", "hey there");
+echo suprem;
+echo $greating;
+
+?>
+
