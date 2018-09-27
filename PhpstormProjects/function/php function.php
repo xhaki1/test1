2c37f24bade5f815c4d39c29a52582bc0a1fcb88
xhaki <turbac2015@gmail.com>
2018-09-27 19:09:29 +0300

    functions

---
+++ PhpstormProjects/function/php function.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: muktar
+ * Date: 27/09/18
+ * Time: 17:49
+ */
+
+
+function add_numbers($eleven=11,$ten=10,$five=5) {
+    $x = $eleven+$ten+$five;
+        echo $x;
+    }
+    add_numbers();
+
+?>
+
+
